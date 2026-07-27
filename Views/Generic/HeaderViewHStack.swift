0370d127e408efe6beecae4cb3ebb372e00d0779
Joe Chan <joechankc2018@yahoo.co.jp>
2026-07-27 17:36:25 +0800

    v1.3.1_20260727

--- Views/Generic/HeaderViewHStack.swift
+++ Views/Generic/HeaderViewHStack.swift
@@ -12,18 +12,51 @@ struct HeaderViewHStack: View {
     @EnvironmentObject var appConfig: AppConfig
 
     var body: some View {
-        HStack {
-            Text(groupKey.code)
-                .font(.headline)
-                .padding(.vertical, 5)
-                .padding(.horizontal)
-                .background(RoundedRectangle(cornerRadius: 10).fill(getColors(for: appConfig.contrast, theme: .normal).backgroundColor))
-                .foregroundColor(getColors(for: appConfig.contrast, theme: .normal).foregroundColor)
+        HStack(spacing: 0) {
+            if #available(iOS 16.0, *) {
+                Text(groupKey.chinese.isEmpty ? groupKey.name : appConfig.enableChinese ? "\(groupKey.name) \(groupKey.chinese)" : groupKey.name)
+                    .font(.subheadline)
+                    .padding(.vertical, 5)
+                    .padding(.horizontal)
+                    .background(
+                        UnevenRoundedRectangle(
+                            topLeadingRadius: 10,
+                            bottomLeadingRadius: 10,
+                            bottomTrailingRadius: 0,
+                            topTrailingRadius: 0
+                        )
+                        .fill(getColors(for: appConfig.contrast, theme: .normal).backgroundColor))
+                    .foregroundColor(getColors(for: appConfig.contrast, theme: .normal).foregroundColor)
             
-            Text(groupKey.chinese.isEmpty ? groupKey.name : appConfig.enableChinese ? "\(groupKey.name) \(groupKey.chinese)" : groupKey.name)
-                .font(.subheadline)
-                .foregroundColor(Color(.secondaryLabel))
+                Text(groupKey.code)
+                    .font(.subheadline)
+                    .padding(.vertical, 5)
+                    .padding(.horizontal)
+                    .background(
+                        UnevenRoundedRectangle(
+                            topLeadingRadius: 0,
+                            bottomLeadingRadius: 0,
+                            bottomTrailingRadius: 10,
+                            topTrailingRadius: 10
+                        )
+                        .fill(getColors(for: appConfig.contrast, theme: .normal).backgroundColor.opacity(0.5)))
+                    .foregroundColor(Color(.secondaryLabel))
+            } else {
+                Text(groupKey.chinese.isEmpty ? groupKey.name : appConfig.enableChinese ? "\(groupKey.name) \(groupKey.chinese)" : groupKey.name)
+                    .font(.subheadline)
+                    .padding(.vertical, 5)
+                    .padding(.horizontal)
+                    .background(RoundedRectangle(cornerRadius: 10).fill(getColors(for: appConfig.contrast, theme: .normal).backgroundColor))
+                    .foregroundColor(getColors(for: appConfig.contrast, theme: .normal).foregroundColor)
+                Text(groupKey.code)
+                    .font(.subheadline)
+                    .padding(.vertical, 5)
+                    .padding(.horizontal)
+                    .background(RoundedRectangle(cornerRadius: 10).fill(getColors(for: appConfig.contrast, theme: .normal).backgroundColor).opacity(0.5))
+                    .foregroundColor(Color(.secondaryLabel))
+            }
         }
+        .padding(.trailing)
     }
 }
 
